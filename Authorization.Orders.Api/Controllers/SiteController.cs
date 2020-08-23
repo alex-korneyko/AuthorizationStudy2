2bf29c4a957919f633e887adbaf5de76b2708b05
alex <a.korneyko@gmail.com>
2020-08-23 21:30:35 +0200

    IdentityServer part 1

---
+++ Authorization.Orders.Api/Controllers/SiteController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Authorization.Orders.Api.Controllers
+{
+    [Route("[controller]")]
+    public class SiteController: Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("[action]")]
+        [Authorize]
+        public string Secret()
+        {
+            return "Secret string from orders API";
+        }
+    }
+}
